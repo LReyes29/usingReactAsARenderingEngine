48266ea81c95173d38bf2c97797f1e6cde440060
Luis Reyes <luisreyessegner@gmail.com>
2019-12-06 21:32:17 +0000

    Exercises Ok

--- exercises/03.3-component-properties/index.js
+++ exercises/03.3-component-properties/index.js
@@ -1,26 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PropType from "prop-types";
+import PropTypes from "prop-types";
 
 const BootstrapCard = props => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/" alt="Card image cap" />
+			<img alt="Card image cap" className="card-img-top" src={props.imageUrl} />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
-				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
-					Go to wikipedia
+				<p className="card-text">{props.description}</p>
+				<a href={props.buttonUrl} className="btn btn-primary">
+					{props.buttonLabel}
 				</a>
 			</div>
 		</div>
 	);
 };
+
 BootstrapCard.propTypes = {
-	title: PropType.string
-		// 2) add here the new properties into the proptypes object
+	title: PropTypes.string.isRequired,
+	imageUrl: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	buttonUrl: PropTypes.string.isRequired,
+	buttonLabel: PropTypes.string.isRequired
 };
 
-
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
+ReactDOM.render(
+	<BootstrapCard
+		title="Bob Dylan"
+		imageUrl="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		buttonLabel="Go to wikipedia"
+	/>,
+	document.querySelector("#myDiv")
+);
--- exercises/05-adding-styles/index.js
+++ exercises/05-adding-styles/index.js
@@ -4,13 +4,16 @@ import PropTypes from "prop-types";
 
 //add the styles here
 const mySuperStyles = {
-
+	background: "black",
+	border: "1px solid yellow",
+	fontSize: "16px"
 };
 
 const Badge = props => {
 	return (
 		<button style={mySuperStyles} type="button" className="btn btn-primary">
-			{props.label}<span className="badge badge-light">{props.number}</span>
+			{props.label}
+			<span className="badge badge-light">{props.number}</span>
 		</button>
 	);
 };
